61e74cbb152cc0dc52cfd3db9d7f16aa6b0f4a76
strykejernene <strykejernene@91351223-0ef9-f92c-83ab-c71e0c8ed5a6>
2011-02-23 15:39:41 +0000

    rtc

--- headers/io.h
+++ headers/io.h
@@ -9,6 +9,7 @@ void LED_initialize( const BITFIELD bits );
 void LED_set_enabled( const BITFIELD bits );
 void AUDIO_initialize();
 void IO_initialize_interrupts();
+void RTC_initialize();
 
 extern Song global_song;
 
